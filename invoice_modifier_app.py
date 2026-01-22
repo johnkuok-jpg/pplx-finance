da55f212140f419ce8e12e93d833ebba16f7cce7
john.kuok <john.kuok@john.kuok-LQH4Y9FYPF>
2026-01-22 14:23:29 -0800

    Fine-tune alignment - move left by 1 point

--- invoice_modifier_app.py
+++ invoice_modifier_app.py
@@ -60,8 +60,8 @@ def modify_invoice_pdf(pdf_bytes, bank_info):
 
     # Add new banking information
     y_start = delete_from_y + 12
-    x_label = 35
-    x_value = 153
+    x_label = 34
+    x_value = 152
     line_height = 12.5
     fontsize = 9
     fontname = "helv"
